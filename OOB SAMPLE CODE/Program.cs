13f2617df4591b893d27375b9fc7c3118327a8d2
Areola John <areolajohnbayo@outlook.com>
2022-08-09 15:11:35 +0100

    Add project files.

---
+++ OOB SAMPLE CODE/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace OOP_SAMPLE_CODE
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Candidate john = new Candidate();
+            Console.WriteLine("Are you a Nigerian? (yes/no)");
+            string nationality = Console.ReadLine().ToLower();
+            Console.WriteLine("Are you above 18?(yes/no)");
+            string age = Console.ReadLine().ToLower();
+            Console.WriteLine("Grade is 2:1 and above? (yes/no)");
+            string grade = Console.ReadLine().ToLower();
+            Console.WriteLine("Score is Seventy and above(yes / no)");
+            string score = Console.ReadLine().ToLower();
+
+            john.IsAgeaboveEighteen= john.Age(age);
+            john.IsNigerian=john.Nationality(nationality);
+            john.IsGradeAboveSeventy=john.Score(score);
+            john.IsSecondUpperAndAbove=john.Grade(grade);
+
+            john.Employ();
+        }
+     }
+}
---
+++ OOB SAMPLE CODE/Weddingpartysample.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace OOP_SAMPLE_CODE
+{
+    public abstract class Employment{
+        private bool  _isNigerian;
+        private bool _isSecondupperAndAbove;
+        private bool _isAgeAboveEighteen;
+        private bool _isGradeAboveSeventy;
+        public bool IsNigerian { 
+            get { return _isNigerian; } 
+            
+            set { _isNigerian = value; } 
+        }
+        public bool IsSecondUpperAndAbove { 
+            get { return _isSecondupperAndAbove; } 
+            set { _isSecondupperAndAbove = value; }
+        }
+        public bool IsAgeaboveEighteen { 
+            get { return _isAgeAboveEighteen; } 
+            set { _isAgeAboveEighteen = value; } 
+        }
+        public bool IsGradeAboveSeventy
+        {
+            get { return _isGradeAboveSeventy; }
+            set { _isGradeAboveSeventy = value; }
+        }
+        public bool Age(string age)
+        {
+            if (age == "yes")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool Grade(string grade)
+        {
+            if (grade == "yes")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool Score(string score)
+        {
+            if (score == "yes")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool Nationality(string nationality)
+        {
+            if (nationality == "yes")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public abstract void Employ();
+
+    }
+
+    public class Candidate:Employment
+    {//Candidate is inheriting the features of employment
+
+        public override void Employ()
+        {
+            if (IsSecondUpperAndAbove && IsNigerian  && IsAgeaboveEighteen && IsGradeAboveSeventy)
+            {
+                Console.WriteLine("Congratulations on the succesful completed of your programme");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, we regret to inform you that we will not be proceeding with your application.");
+            }
+        }
+
+    }
+}
